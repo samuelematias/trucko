a7174477d3dc48171be9136a905c41799a27c561
samuelematias <hi@samuelematias.com>
2020-06-14 11:51:06 -0300

    create Routes

---
+++ lib/src/core/routing/routes.dart
@@ -0,0 +1,7 @@
+class AppTabNavigatorRoutes {
+  static const String root = '/';
+  static const String login = '/login';
+  static const String companies = '/companies';
+  static const String map = '/map';
+  static const String rewards = '/rewards';
+}
