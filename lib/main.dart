c626a31d12589e24a061546a7afe5b377b109c9e
samuelematias <hi@samuelematias.com>
2020-06-14 06:25:48 -0300

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:trucko/src/core/theme/theme.dart';
-import 'package:trucko/src/ui/widgets/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:trucko/src/ui/pages/login/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,77 +9,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    //Transparent statusBar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Trucko',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ).h1(
-                color: Colors.red, fontSize: 18, fontWeight: FontWeight.normal),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: Spacing.ms),
-              child: PrimaryButton(
-                onPressed: () {},
-                label: 'ENTRAR COM FACEBOOK',
-                loading: true,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(Spacing.ms),
-              child: SecondaryButton(
-                onPressed: () {},
-                labelLeft: 'PARTICIPAR',
-                labelRight: '+20 pts',
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: LoginPage(),
     );
   }
 }
