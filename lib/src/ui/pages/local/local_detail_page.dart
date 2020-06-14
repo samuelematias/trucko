0d8c91509fdd2dafee276cf58a0301d6c66edef1
samuelematias <hi@samuelematias.com>
2020-06-14 14:11:20 -0300

    create _buildTruckersAroundList

--- lib/src/ui/pages/local/local_detail_page.dart
+++ lib/src/ui/pages/local/local_detail_page.dart
@@ -8,6 +8,50 @@ class LocalDetailPage extends StatefulWidget {
 }
 
 class _LocalDetailPageState extends State<LocalDetailPage> {
+  final List<Map<String, dynamic>> truckersAround = [
+    {
+      "name": "Samuel",
+      "photo": "https://i.imgur.com/4kGRWdN.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0f",
+      "crown": true,
+    },
+    {
+      "name": "Pedro",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0a",
+      "crown": false,
+    },
+    {
+      "name": "Biel",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0b",
+      "crown": false,
+    },
+    {
+      "name": "Bolsoni",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0c",
+      "crown": false,
+    },
+    {
+      "name": "Barroso",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0c",
+      "crown": false,
+    },
+    {
+      "name": "João",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0c",
+      "crown": false,
+    },
+    {
+      "name": "Jho n",
+      "photo": "https://i.imgur.com/JKy7cll.jpg",
+      "id": "6d579be3-68b8-454f-9a5f-f7b3aaed1a0e",
+      "crown": false,
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +86,7 @@ class _LocalDetailPageState extends State<LocalDetailPage> {
                 _buildLogo(),
                 _buildLocalName(),
                 _buildAlertTruckersAround(count: 4),
+                _buildTruckersAroundList(truckersAround),
               ],
             ),
           )
@@ -64,7 +109,12 @@ class _LocalDetailPageState extends State<LocalDetailPage> {
         ),
       );
 
-  Widget _buildLogo() => RoundImage();
+  Widget _buildLogo() => RoundImage(
+        width: 80.0,
+        height: 80.0,
+        imageUrl:
+            'https://lh3.googleusercontent.com/proxy/udNKxr0AEaZD6qSrZ7HUnYaHRkH7x38boN3Jzfmri851cJvL9B7MguksjBKuoMxZvtvH4GWgpUUXOGohtRnectXLoax1JJbJFNmSJbGW-was',
+      );
 
   Widget _buildLocalName() => Padding(
         padding: EdgeInsets.fromLTRB(
@@ -88,4 +138,40 @@ class _LocalDetailPageState extends State<LocalDetailPage> {
         child: Text('$count amigos caminhoneiros estão aqui agora!')
             .p1(color: ColorPalette.green50),
       );
+
+  Widget _buildTruckersAroundList(List<Map<String, dynamic>> truckersAround) {
+    return Container(
+      height: 60.0,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: truckersAround.length,
+        itemBuilder: (BuildContext context, int index) {
+          final trucker = truckersAround.elementAt(index);
+          return _buildTruckersAroundListItem(trucker, truckersAround, index);
+        },
+      ),
+    );
+  }
+
+  Widget _buildTruckersAroundListItem(
+    Map<String, dynamic> trucker,
+    List<Map<String, dynamic>> truckersAround,
+    int index,
+  ) {
+    final bool isTheFirstPositionOfArray = index == 0;
+    final bool isTheLastPositionOfArray = index + 1 == truckersAround.length;
+    return Padding(
+      padding: EdgeInsets.only(
+        left: isTheFirstPositionOfArray ? Spacing.l : 0.0,
+        right: isTheLastPositionOfArray ? Spacing.l : Spacing.s,
+      ),
+      child: RoundImage(
+        borderColor: ColorPalette.green50,
+        widthBorderImage: 3,
+        imageUrl: trucker['photo'],
+        showBadge: trucker['crown'],
+      ),
+    );
+  }
 }
