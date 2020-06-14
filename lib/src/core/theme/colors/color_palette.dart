acae59ff98258e114577cd7d774e05b9def14deb
samuelematias <hi@samuelematias.com>
2020-06-14 04:56:50 -0300

    create ColorPalette

---
+++ lib/src/core/theme/colors/color_palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  ColorPalette._();
+
+  static const Color transparent = Color(0x00000000);
+  static const Color red50 = Color(0xFFFF725E);
+  static const Color blue50 = Color(0xFF375EE5);
+  static const Color blue100 = Color(0xFF2F80ED);
+  static const Color green50 = Color(0xFF39B06C);
+  static const Color black50 = Color(0xFF000000);
+  static const Color black100 = Color(0xFF333333);
+  static const Color white50 = Color(0xFFFFFFFF);
+  static const Color grey50 = Color(0xFFF2F2F2);
+  static const Color grey100 = Color(0xFF828282);
+  static const Color grey200 = Color(0xFF757575);
+  static const Color orange50 = Color(0xFFFF7752);
+  static const Color mustard50 = Color(0xFFE7A74E);
+}
