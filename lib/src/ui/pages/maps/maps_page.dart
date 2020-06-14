f5f4f414b8f9fc721b24d47c8d493bd50b715083
samuelematias <hi@samuelematias.com>
2020-06-14 11:46:32 -0300

    create new pages

---
+++ lib/src/ui/pages/maps/maps_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapsPage extends StatefulWidget {
+  @override
+  _MapsPageState createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Maps'),
+    );
+  }
+}
---
+++ lib/src/ui/pages/rewards/rewards_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RewardsPage extends StatefulWidget {
+  @override
+  _RewardsPageState createState() => _RewardsPageState();
+}
+
+class _RewardsPageState extends State<RewardsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Rewards'),
+    );
+  }
+}
