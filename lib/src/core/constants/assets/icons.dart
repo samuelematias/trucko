2217207e9595f4f35dfdd5cc49df4cc32b7fd81a
samuelematias <hi@samuelematias.com>
2020-06-14 04:47:13 -0300

    add icons

---
+++ lib/src/core/constants/assets/icons.dart
@@ -0,0 +1,9 @@
+import 'package:trucko/src/core/constants/assets/prefix.dart';
+
+const String iconMapActive = '${PREFIX}icons/icon-map-active.png';
+const String iconMapInactive = '${PREFIX}icons/icon-map-inactive.png';
+const String iconRewardActive = '${PREFIX}icons/icon-reward-active.png';
+const String iconRewardInactive = '${PREFIX}icons/icon-reward-inactive.png';
+const String iconMegaphoneActive = '${PREFIX}icons/icon-megaphone-active.png';
+const String iconMegaphoneInactive =
+    '${PREFIX}icons/icon-megaphone-inactive.png';
