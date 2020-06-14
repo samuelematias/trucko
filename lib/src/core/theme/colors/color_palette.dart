3104f2f542edfc291c06e3c53940c6715f1f26cf
samuelematias <hi@samuelematias.com>
2020-06-14 13:12:22 -0300

    add new colors

--- lib/src/core/theme/colors/color_palette.dart
+++ lib/src/core/theme/colors/color_palette.dart
@@ -4,6 +4,8 @@ class ColorPalette {
   ColorPalette._();
 
   static const Color transparent = Color(0x00000000);
+  static const Color overlay50 = Color.fromRGBO(0, 0, 0, 0.24);
+  static const Color overlay100 = Color.fromRGBO(0, 0, 0, 0.35);
   static const Color red50 = Color(0xFFFF725E);
   static const Color blue50 = Color(0xFF375EE5);
   static const Color blue100 = Color(0xFF2F80ED);
@@ -15,6 +17,7 @@ class ColorPalette {
   static const Color grey100 = Color(0xFF828282);
   static const Color grey200 = Color(0xFF757575);
   static const Color grey300 = Color(0xFFBDBDBD);
+  static const Color grey400 = Color(0xFFE0E0E0);
   static const Color orange50 = Color(0xFFFF7752);
   static const Color mustard50 = Color(0xFFE7A74E);
 }
