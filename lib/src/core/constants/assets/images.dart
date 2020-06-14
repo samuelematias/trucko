f2a0e198d9f12eb5b5fc4bada5fd2f63f3658f9f
samuelematias <hi@samuelematias.com>
2020-06-14 16:13:37 -0300

    add new images

--- lib/src/core/constants/assets/images.dart
+++ lib/src/core/constants/assets/images.dart
@@ -2,3 +2,5 @@ import 'package:trucko/src/core/constants/assets/prefix.dart';
 
 const String trucko = '${PREFIX}trucko.png';
 const String imgTrucko = '${PREFIX}img-trucko.png';
+const String imgCompanies1 = '${PREFIX}img-companies-1.png';
+const String imgCompanies2 = '${PREFIX}img-companies-2.png';
