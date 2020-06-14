6bb5ec4d89c6a88d390b358f9a973785a7b20f59
samuelematias <hi@samuelematias.com>
2020-06-14 14:11:01 -0300

    improving RoundImage widget

--- lib/src/ui/widgets/images/round_image.dart
+++ lib/src/ui/widgets/images/round_image.dart
@@ -1,36 +1,63 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:trucko/src/core/constants/assets/assets.dart';
 import 'package:trucko/src/core/theme/theme.dart';
 
 class RoundImage extends StatelessWidget {
-  final String image;
+  final String imageUrl;
   final double widthBorderImage;
   final Color borderColor;
   final double width;
   final double height;
+  final bool showBadge;
 
   const RoundImage({
     Key key,
-    this.image,
+    this.imageUrl = '',
     this.width = 48.0,
     this.height = 48.0,
     this.widthBorderImage = 2.0,
     this.borderColor = ColorPalette.grey400,
+    this.showBadge = false,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: borderColor, width: widthBorderImage),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(
-              image ?? imgTrucko,
+  Widget build(BuildContext context) => Stack(
+        children: [
+          Container(
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: borderColor, width: widthBorderImage),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: imageUrl.isNotEmpty
+                    ? CachedNetworkImageProvider(imageUrl)
+                    : AssetImage(
+                        iconMapInactive,
+                      ),
+              ),
             ),
           ),
-        ),
+          Visibility(
+            visible: showBadge,
+            child: Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                width: 16.0,
+                height: 16.0,
+                decoration: BoxDecoration(
+                  color: ColorPalette.red50,
+                  shape: BoxShape.circle,
+                ),
+                child: Image(
+                  image: AssetImage(iconCrown),
+                ),
+              ),
+            ),
+          ),
+        ],
       );
 }
